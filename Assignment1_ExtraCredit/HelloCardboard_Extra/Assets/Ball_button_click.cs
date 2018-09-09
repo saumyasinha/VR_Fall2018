ab2327d0ff5bc4301a4042d7f72fdf7feca4223d
Saumya Sinha <saumya.somu09@gmail.com>
2018-09-08 20:03:46 -0600

    Add final game for extra credit

---
+++ Assignment1_ExtraCredit/HelloCardboard_Extra/Assets/Ball_button_click.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball_button_click : MonoBehaviour {
+
+	public Rigidbody rb;
+	public Text winText;
+	// Use this for initialization
+	void Start () {
+		winText.text = "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			
+
+//			rb.GetComponent<Rigidbody> ().AddForce (Camera.main.transform.forward * Random.Range (500, 750));
+			rb.GetComponent<Rigidbody> ().useGravity=true;
+		}
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.transform.tag == "Target")
+		{
+			other.gameObject.SetActive(false);
+			winText.text = "WHAMMM!!";
+		}
+
+	}
+
+
+}
